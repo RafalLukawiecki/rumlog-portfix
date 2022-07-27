cb143d4e4e2e200e0c7f98462688182acc9b3fea
Rafal Lukawiecki <rafal@projectbotticelli.com>
2022-07-27 16:27:26 +0100

    Added MIT licence

--- rumlog-portfix.sh
+++ rumlog-portfix.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 #set -x
 
-# Rafal Lukawiecki EI6LA raf@rafal.net
 # RLL 27JUL22
 #
 # Discover the CAT port for the Yaesu device currently in use by RumLogNG and set it into its
 # config. This script can be adapted to cater for other transceivers if needed. Please get in
 # touch if needed.
+
+# MIT License
+
+# Copyright (c) 2022 Rafal Lukawiecki
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 # Required software
 RIGCTL=${RIGCTL:-"rigctl"}
